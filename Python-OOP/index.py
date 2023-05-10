dc27a0f019099232517aae7dd18c7b975f312819
soudomsinh <sengdaosinh@gmail.com>
2023-05-10 07:42:09 -0400

    Update class/method/attribute/obeject

--- Python-OOP/index.py
+++ Python-OOP/index.py
@@ -5,9 +5,12 @@ class Employee():
         self.name = name                    # this is attribute
         self.salary = salary                 # this is attribute
         self.department = department          # this is attribute
-        print("Name = ", name)
+
+    def showData(self):
+        print("name = {} ".format(self.name)) # Another way to do it
         print("Salary = {} ".format(self.salary)) # Another way to do it
-        print("Department = ", department)
+        print("Department = {} ".format(self.department)) # Another way to do it
+
 
 # creating object -> obj1
 obj1 = Employee()
@@ -19,3 +22,8 @@ obj2.detail("Sara", 4000, "Accounting" )
 
 obj3 = Employee()
 obj3.detail("Tommy", 3000, "Designer" )
+
+obj1.showData()
+obj2.showData()
+obj3.showData()
+
